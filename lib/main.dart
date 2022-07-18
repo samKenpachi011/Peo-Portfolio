29d92684ef25bb5a469545824a6511b3c662df3f
samKenpachi011 <samuelkabelo1@gmail.com>
2022-07-18 22:17:25 +0200

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(const MyApp());
+// void main() => runApp(const MyApp());
+void main() async{
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -9,7 +14,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home:  Text('Peo App'),
+      title: 'Peo App',
+      home: HomePage(),
+      );
+  }
+
+}
+
+class HomePage extends StatefulWidget{
+  const HomePage({Key? key}) : super(key: key);
+
+  // final String title;
+  @override
+  _HomePageState createState() => _HomePageState();
+
+}
+
+class _HomePageState extends State<HomePage>{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:  AppBar(
+        title: const Text('Peo App'),
+      ),
+      body: const Center(
+        child: Text('Peo App Development'),
+      ),
     );
   }
 }
